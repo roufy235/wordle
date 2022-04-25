6690c10093a9e4151d1f17cc0e9aac67623fdcf4
roufy235 <roufy235@gmail.com>
2022-04-25 23:30:01 +0100

    feat: update letters attribute

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,37 @@
-console.log('HELLO');
 
-const test = () => {
-	console.log('this is a test');
-};
+// detect keypress
+const lettersPattern  = /^[A-Za-z][A-Za-z0-9]*$/;
+let currentGuessCount = 1;
+let currentGuess = document.querySelector('#guess' + currentGuessCount);
+let currentLetters = currentGuess.dataset.letters
+
+document.addEventListener('keydown', (e) => {
+	const keypress = e.key
+	// if its a letter
+	 if (lettersPattern.test(keypress) && keypress.length === 1) {
+		updateLetters(keypress)
+	}
+	// if backspace
+	if(keypress.toLocaleLowerCase === 'backspace') {
+		console.log('backspace pressed');
+	}
+})
+
+// update "letters"
+const updateLetters = (letter) => {
+	let oldLetters = currentGuess.dataset.letters
+	let newLetters = oldLetters + letter
+	currentGuess.dataset.letters = newLetters
+	let currentTile = newLetters.length;
+	if(currentTile <= 5) {
+		updateTiles(currentTile, letter);
+	}
+}
+
+// update tile markup
+const updateTiles = (tileNumber, letter) => {
+	let currentTile = document.querySelector('#guessTile' + tileNumber);
+	currentTile.innerText = letter;
+}
+
+// delete last letter
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("HELLO");var test=function(){console.log("this is a test")};
+"use strict";var lettersPattern=/^[A-Za-z][A-Za-z0-9]*$/,currentGuessCount=1,currentGuess=document.querySelector("#guess"+currentGuessCount),currentLetters=currentGuess.dataset.letters;document.addEventListener("keydown",(function(e){var t=e.key;lettersPattern.test(t)&&1===t.length&&updateLetters(t),"backspace"===t.toLocaleLowerCase&&console.log("backspace pressed")}));var updateLetters=function(e){var t=currentGuess.dataset.letters+e;currentGuess.dataset.letters=t;var s=t.length;s<=5&&updateTiles(s,e)},updateTiles=function(e,t){document.querySelector("#guessTile"+e).innerText=t};
 //# sourceMappingURL=script.js.map
